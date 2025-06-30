0f6bcb125e873add1892948f68a944ac4227e74e
Moyida-Prameela <prameelamoyida@gmail.com>
2025-06-30 18:18:41 +0530

    Initial commit with project files

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaysOrder from './pages/PlaysOrder/PlaysOrder'
+import Footer from './components/Footer/Footer'
+import LoginPopUp from './components/LoginSignUp/LoginPopUp'
+
+const App = () => {
+  const [showLogin,setShowLogin]=useState(false)
+  return (
+    <>
+    {showLogin?<LoginPopUp setShowLogin={setShowLogin}/>:<></>}
+      <div className='app'>
+      <Navbar setShowLogin={setShowLogin}/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/order' element={<PlaysOrder/>}/>
+      </Routes>
+    </div>
+    <Footer/>
+    </>
+     
+    
+  )
+}
+
+export default App
